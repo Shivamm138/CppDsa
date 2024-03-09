1e907db18eced548b72e28be894d9aef8176e156
Shivamm138 <shivamcode138@gmail.com>
2024-03-10 01:44:54 +0530

    bubble and insertion sort

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -19,6 +19,37 @@ void selection_sort(int arr[], int n)
     }
 }
 
+void bubble_sort(int arr[], int n ){
+    for (int i = n - 1; i >= 0; i--){
+        int didSwap = 0;
+        for (int j = 0; j <= i - 1; j++){
+            if(arr[j]> arr[j+1]){
+                int temp = arr[j + 1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+                didSwap = 1;
+            }
+        }
+        if(didSwap==0){
+            break;
+        }
+    }
+}
+
+void Insertion_sort(int arr[], int n){
+    for (int i = 0; i <= n - 1; i++){
+        int j = i;
+        while (j > 0 && arr[j-1]> arr[j]){
+            int temp = arr[j - 1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+
+                j--;
+        }
+
+    }
+}
+
 int main()
 {
     int n;
@@ -26,7 +57,7 @@ int main()
     int arr[n];
     for (int i = 0; i < n; i++)
         cin >> arr[i];
-    selection_sort(arr, n);
+    Insertion_sort(arr, n);
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
